22743413d7d35d766e07bd8cb7abc40b35a413bc
Ziol3k <jan_ziolo@wp.pl>
2024-01-08 23:52:50 +0100

    !!!!

--- js/stanZalogowania.js
+++ js/stanZalogowania.js
@@ -5,12 +5,19 @@ function updateUIBasedOnLogin() {
   const myProfile = document.getElementById('myProfile');
   const logoutButton = document.getElementById('logoutButton');
 
+  const addLoggedOut = document.getElementById('addLoggedOut');
+  const addLoggedIn = document.getElementById('addLoggedIn');
+
   if (loggedInUser) {
     loginRegister.style.display = 'none';
     myProfile.style.display = 'block';
+    addLoggedIn.style.display = 'none';
+    addLoggedOut.style.display = 'block';
   } else {
     loginRegister.style.display = 'block';
     myProfile.style.display = 'none';
+    addLoggedIn.style.display = 'block';
+    addLoggedOut.style.display = 'none';
   }
 
   if (logoutButton) {
